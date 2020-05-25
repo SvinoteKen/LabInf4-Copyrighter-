63b1d7bd76e410a719fad08806c56f0087349ad5
свин свин <stastab2013@gmail.com>
2020-05-25 16:44:42 +0300

    Add project files.

---
+++ LabInf4/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LabInf4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.operationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyrightTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyrightDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.operationsToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(907, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.openToolStripMenuItem1,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.fileToolStripMenuItem.Text = " File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(159, 22);
+            this.openToolStripMenuItem1.Text = "Open dictionary";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.OpenToolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(156, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // operationsToolStripMenuItem
+            // 
+            this.operationsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyrightTextToolStripMenuItem,
+            this.copyrightDirectoryToolStripMenuItem});
+            this.operationsToolStripMenuItem.Name = "operationsToolStripMenuItem";
+            this.operationsToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.operationsToolStripMenuItem.Text = "Operations";
+            // 
+            // copyrightTextToolStripMenuItem
+            // 
+            this.copyrightTextToolStripMenuItem.Name = "copyrightTextToolStripMenuItem";
+            this.copyrightTextToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.copyrightTextToolStripMenuItem.Text = "Copyright text";
+            this.copyrightTextToolStripMenuItem.Click += new System.EventHandler(this.CopyrightTextToolStripMenuItem_Click);
+            // 
+            // copyrightDirectoryToolStripMenuItem
+            // 
+            this.copyrightDirectoryToolStripMenuItem.Name = "copyrightDirectoryToolStripMenuItem";
+            this.copyrightDirectoryToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.copyrightDirectoryToolStripMenuItem.Text = "Copyright directory";
+            this.copyrightDirectoryToolStripMenuItem.Click += new System.EventHandler(this.CopyrightDirectoryToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontToolStripMenuItem,
+            this.colorToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.FontToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.ColorToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.InfoToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 491);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(218, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(677, 171);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 185;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(218, 204);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(386, 314);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(631, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Copyright";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(631, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 95);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(631, 439);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 79);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Batch mode ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "check-mark-bottle-material-green-tick.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 530);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem operationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private DataGridView dataGridView1;
+        private PictureBox pictureBox1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ToolStripMenuItem copyrightTextToolStripMenuItem;
+        private ToolStripMenuItem copyrightDirectoryToolStripMenuItem;
+        private ImageList imageList1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private ToolStripMenuItem fontToolStripMenuItem;
+        private FontDialog fontDialog1;
+        private ColorDialog colorDialog1;
+        private ToolStripMenuItem colorToolStripMenuItem;
+        private ToolStripMenuItem infoToolStripMenuItem;
+    }
+}
+
---
+++ LabInf4/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabInf4
+{
+    public partial class Form1 : Form
+    {
+        List<string> images = new List<string>();
+        Bitmap panelBitmap;
+        Bitmap selcetedImage;
+        int selectedPicture = 0;
+        string copyrightText = "";
+        string directoryForSave = "";
+        Font font = new Font("Times New Roman", 100);
+        Brush brush = Brushes.Red;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "image files(*.PNG;*.JPG;*.TIFF;*.PSD;*.BMP)|*.PNG;*.JPG;*.TIFF;*.PSD;*.BMP";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = null;
+                images.Clear();
+                selectedPicture = 0;
+                panelBitmap = new Bitmap(panel1.Width, 90 + 15);
+                panel1.AutoScrollMinSize = new Size(0, 90 + 15);
+                images.Add(openFile.FileName);
+                Bitmap image = new Bitmap(openFile.FileName);
+                Graphics graphics = Graphics.FromImage(panelBitmap);
+                graphics.DrawImage(image, 10, 15, 90, 90);
+                panel1.Refresh();
+                if (images.Count > 0)
+                {
+                    selcetedImage = new Bitmap(images[0]);
+                    pictureBox1.Image = selcetedImage;
+                }
+            }
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Transform = new Matrix(1, 0, 0, 1, panel1.AutoScrollPosition.X, panel1.AutoScrollPosition.Y);
+            if (panelBitmap != null)
+            {
+                e.Graphics.DrawImage(panelBitmap, 0,0);
+            }
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int realY = e.Y - panel1.AutoScrollPosition.Y;
+            int cgeck = (realY - 15) / 90;
+            if (cgeck >= images.Count)
+            {
+                return;
+            }
+            selectedPicture = cgeck;
+            Bitmap image = new Bitmap(images[selectedPicture]);
+            pictureBox1.Image = image;
+        }
+
+        private void OpenToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            images.Clear();
+            pictureBox1.Image = null;
+
+            FolderBrowserDialog file = new FolderBrowserDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                int i = 0;
+
+                foreach (string imagefile in Directory.EnumerateFiles(file.SelectedPath))
+                {
+                    try
+                    {
+                        Bitmap image = new Bitmap(imagefile);
+                        images.Add(imagefile);
+                        i++;
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+                panelBitmap = new Bitmap(panel1.Width, 90 * i + 15);
+                panel1.AutoScrollMinSize = new Size(0, 90 * i + 15);
+                i = 0;
+
+                foreach (string files in Directory.EnumerateFiles(file.SelectedPath))
+                {
+                    try
+                    {
+                        Bitmap image = new Bitmap(files);
+
+                        Graphics graphics = Graphics.FromImage(panelBitmap);
+                        graphics.DrawImage(image , 10, 90 * i + 15, 90, 90);
+                        i++;
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+                panel1.Refresh();
+                if (images.Count > 0)
+                {
+                    selcetedImage = new Bitmap(images[0]);
+                    pictureBox1.Image = selcetedImage;
+                }
+
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void CopyrightDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Укажите директорию для сохранения картинок");
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                directoryForSave = fbd.SelectedPath;
+            }
+        }
+
+        private void CopyrightTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Введите текст копирайта в текст бокс");
+            Form2 f2 = new Form2();
+            if (f2.ShowDialog() == DialogResult.OK)
+            {
+                copyrightText = f2.textBox1.Text;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (copyrightText == "")
+            {
+                MessageBox.Show("Укажите текст для копирайта в Settings - Copyright text");
+                return;
+            }
+            if (images.Count == 0)
+            {
+                return;
+            }
+            selcetedImage = new Bitmap(images[selectedPicture]);
+            Graphics graphics = Graphics.FromImage(selcetedImage);
+            graphics.DrawString(copyrightText, font, brush, new Point(80, selcetedImage.Height - 350));
+            pictureBox1.Image = selcetedImage;
+            Bitmap image = new Bitmap(imageList1.Images[0]);
+            Graphics graphics1 = Graphics.FromImage(panelBitmap);
+            graphics1.DrawImage(image, 70, 90 * (selectedPicture + 1) - 15, 30, 30);
+            panel1.Refresh();
+            FileInfo file = new FileInfo(images[selectedPicture]);
+            dataGridView1.Rows.Add(file.Name, selcetedImage.Width, selcetedImage.Height, copyrightText + $" [{DateTime.Now.ToShortTimeString()}]");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.DefaultExt = ".jpg";
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(sf.FileName);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (directoryForSave == "")
+            {
+                MessageBox.Show("Укажите деррикторию для сохранения картинок в Setting - Copyright directory");
+                return;
+            }
+            if (copyrightText == "")
+            {
+                MessageBox.Show("Укажите текст для копирайта в Settings - Copyright text");
+                return;
+            }
+            for (int j = 0; j < images.Count; j++)
+            {
+                selcetedImage = new Bitmap(images[j]);
+                Graphics graphics = Graphics.FromImage(selcetedImage);
+                graphics.DrawString(copyrightText, font, Brushes.Red, new Point(80, selcetedImage.Height - 350));
+                selcetedImage.Save(directoryForSave + $"\\LabImage{j}.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                Bitmap image = new Bitmap(imageList1.Images[0]);
+                Graphics ds = Graphics.FromImage(panelBitmap);
+                ds.DrawImage(image, 70, 90 * (j + 1) - 15, 30, 30);
+                panel1.Refresh();
+                FileInfo info = new FileInfo(images[j]);
+                dataGridView1.Rows.Add(info.Name, selcetedImage.Width, selcetedImage.Height, copyrightText + $" [{DateTime.Now.ToShortTimeString()}]");
+                pictureBox1.Image = selcetedImage;
+                selectedPicture = images.Count - 1;
+            }
+        }
+        private void FontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            Font = fontDialog1.Font;
+        }
+
+        private void ColorToolStripMenuItem_Click(object sender, EventArgs e) //я хотел сделать что бы можно было выбрать цвет для копирайта но Color нельзя перенести в Brush
+        {
+            colorDialog1.ShowDialog();
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Style.BackColor = colorDialog1.Color;
+                }
+            }
+        }
+
+        private void InfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Приложение, позволяющее открывать указанное изображение или все изображения в указанной директории.Сохранять новую версию изображения с добавленным текстом копирайта.");
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete && selectedPicture < images.Count)
+            {
+                if (panelBitmap != null)
+                {
+                    images.RemoveAt(selectedPicture);
+                    int i = images.Count;
+                    panelBitmap = new Bitmap(panel1.Width, 90 * i + 15);
+                    panel1.AutoScrollMinSize = new Size(0, 90 * i + 15);
+
+                    i = 0;
+
+                    foreach (string files in images)
+                    {
+                        try
+                        {
+                            Bitmap image = new Bitmap(files);
+
+                            Graphics graphics = Graphics.FromImage(panelBitmap);
+                            graphics.DrawImage(image, 10, 90 * i + 15, 90, 90);
+                            i++;
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                    }
+                    panel1.Refresh();
+                    pictureBox1.Image = null;
+                }
+            }
+        }
+        
+    }
+}
